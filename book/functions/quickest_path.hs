7f48f59ba6aea41e6d060730977d4e658d492cc4
JankDev <robert.s.kraut@gmail.com>
2020-04-26 12:04:08 +0200

    Quickest path

--- book/functions/quickest_path.hs
+++ book/functions/quickest_path.hs
@@ -1,15 +1,22 @@
 import Data.List
 
+data CrossRoad = CrossRoad Int Int Int deriving(Show)
+
 main = do
       contents <- getContents
-      let groups = groupsOf 3 $ map read $ lines contents
+      let groups = map (\[a,b,c] -> CrossRoad a b c) . groupsOf 3 $ map read $ lines contents
       putStrLn $ show groups
       putStrLn $ show (quickestPath groups)
 
-quickestPath :: [[Int]] -> [Int]
+quickestPath :: [CrossRoad] -> [Int]
 quickestPath = minimumBy (\u d -> compare (sum u) (sum d)) . foldl func [[],[]]
-      where func [u,d] (a:b:c:xs) = case minimum [a,b,a+c,b+c] of a = u++[a]
-                                                                  b = d++[]
+            where func [u,d] (CrossRoad a b c)
+                              | min ((sum u) + a) ((sum d) + b+c) == ((sum u) + a) && min ((sum d) + b) ((sum u)+ a+c) == (sum d) + b = [u++[a],d++[b]]
+                              | min ((sum u) + a) ((sum d) + b+c) == (sum d) +b+c && min ((sum d) + b) ((sum u)+ a+c) == (sum d) + b = [d++[b,c],d++[b]]
+                              | min ((sum u) + a) ((sum d) + b+c) == (sum d) +b+c && min ((sum d) + b) ((sum u)+ a+c) == (sum u) + a + c = [d++[b,c],u++[a,c]]
+                              | otherwise = [u++[a],u++[a,c]]
+ 
+            
 
 
 groupsOf :: Int -> [Int] -> [[Int]]
